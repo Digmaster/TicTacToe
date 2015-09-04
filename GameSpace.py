88494a0b82df87536c4ee05bbbcb8cd5bb4b2737
Tom <ThomasLuppi@gmail.com>
2015-09-04 16:41:15 -0400

    Move to Pygame
    Visualiser! Only works for 2 agents mode

---
+++ GameSpace.py
@@ -0,0 +1,93 @@
+import pygame
+from pygame.locals import *
+from Model import GameBoard
+from Agent import SmartAgent as Agent
+import threading
+from copy import deepcopy
+
+class GameSpace:
+	resourcesBase = "resources/"
+	xChar = resourcesBase+"xChar.png"
+	oChar = resourcesBase+"oChar.png"
+	bChar = resourcesBase+"bChar.png"
+
+	def __init__(self, board, numAgents):
+		pygame.init()
+
+		self.size = self.width, self.height = 64*3, 64*3
+		self.black = 0, 0, 0
+		self.screen = pygame.display.set_mode(self.size)
+
+		self.board = board
+
+		self.moveNum = 0
+
+		if(numAgents >= 1):
+			self.agent1 = Agent(board)
+		if(numAgents == 2):
+			self.agent2 = Agent(board)
+
+		self.aiJob = False
+		self.input = ""
+
+	def update_screen(self):
+		self.screen.fill(self.black)
+		for i in range(3):
+			for j in range(3):
+				char = self.board.getSquareCoordinates(i, j)
+				charSource = ""
+				if(char=='X'):
+					charSource = self.xChar
+				elif(char=='O'):
+					charSource = self.oChar
+				else:
+					charSource = self.bChar
+				img = pygame.image.load(charSource)
+				rec = img.get_rect()
+				rec.top = i*64
+				rec.left = j*64
+
+				self.screen.blit(img, rec)
+		pygame.display.update()
+
+	def RunAI(self, agent, board, player):
+		self.input = agent.getNextMove(player)
+		self.aiJob = False
+
+
+	def main(self):
+		while(self.board.testWin() is '.'):
+			self.update_screen()
+			player = (self.moveNum%2) + 1
+			move = None
+			if(player == 1):
+				if(self.agent1 != None):
+					if(self.aiJob==False and self.input==""):
+						self.aiJob=True
+						t = threading.Thread(target=self.RunAI, args=(self.agent1, deepcopy(self.board), 'X'))
+						t.start()
+					elif(self.aiJob==False):
+						move = self.input
+						self.input = ""
+				else:
+					move = input(moveStatement.format(moveNum, player, ""))
+			else:
+				if(self.agent2 != None):
+					if(self.aiJob==False and self.input==""):
+						self.aiJob=True
+						t = threading.Thread(target=self.RunAI, args=(self.agent2, deepcopy(self.board), 'O'))
+						t.start()
+					elif(self.aiJob==False):
+						move = self.input
+						self.input = ""
+				else:
+					move = input(moveStatement.format(moveNum, player, ""))
+
+			try:
+				if(move!=None):
+					self.board.setSquare(move, 'X' if player==1 else 'O')
+					self.moveNum = self.moveNum+1
+			except ValueError as e:
+				print e
+		self.update_screen();
+		
--- TicTacToe.py
+++ TicTacToe.py
@@ -1,7 +1,11 @@
 from Model import GameBoard
 from Agent import SmartAgent as Agent
+import pygame
+from pygame.locals import *
+from GameSpace import GameSpace
 
 moveStatement = "Move #{}: Player {} plays {}"
+DESIRED_FPS = 60.0 # 60 frames per second
 
 agents = input("Enter Choice (1 for single agent 2 for dual agents): ")
 agent1 = None
@@ -9,34 +13,14 @@ agent2 = None
 
 board = GameBoard()
 
-if(agents >= 1):
-	agent1 = Agent(board)
-if(agents == 2):
-	agent2 = Agent(board)
-moveNum = 0
-while(board.testWin() is '.'):
-	print board
-	player = (moveNum%2) + 1
-	move = 0
-	if(player == 1):
-		if(agent1 != None):
-			move = agent1.getNextMove('X')
-			print moveStatement.format(moveNum, player, move)
-		else:
-			move = input(moveStatement.format(moveNum, player, ""))
-	else:
-		if(agent2 != None):
-			move = agent2.getNextMove('O')
-			print moveStatement.format(moveNum, player, move)
-		else:
-			move = input(moveStatement.format(moveNum, player, ""))
+# if __name__ == '__main__':
+# 	gs = GameSpace()
+# 	tick = LoopingCall(gs.main)
+# 	tick.start(1.0 / DESIRED_FPS)
 
-	try:
-		board.setSquare(move, 'X' if player==1 else 'O')
-		moveNum = moveNum+1
-	except ValueError as e:
-		print e
-print board
+gs = GameSpace(board, agents)
+gs.main()
+
 if(board.testWin()=='T'):
 	print "The game ends in a tie!"
 else:
